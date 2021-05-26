52adacbcf4f93361296925feab1ce0ac703747f3
Alexou80 <84702060+Alexou80@users.noreply.github.com>
2021-05-26 16:30:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <a href="https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F9%2F99%2FUnofficial_JavaScript_logo_2.svg%2F1200px-Unofficial_JavaScript_logo_2.svg.png&imgrefurl=https%3A%2F%2Ffr.wikipedia.org%2Fwiki%2FJavaScript&tbnid=imsyna4i1ZfCQM&vet=12ahUKEwj4oJ2-g-fwAhV30LsIHcTJBqgQMygAegUIARDOAQ..i&docid=0pWrSMi03VxtNM&w=1200&h=1200&q=logo%20js&ved=2ahUKEwj4oJ2-g-fwAhV30LsIHcTJBqgQMygAegUIARDOAQ"> </a> 
+      <img src={Logo} className="app-logo" alt="logo"></img>
       </header>
+   
     </div>
   );
 }
