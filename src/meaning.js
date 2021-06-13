dea99a2b465ebe700d5327024aa1c216970068dc
Alexou80 <84702060+Alexou80@users.noreply.github.com>
2021-06-13 09:40:50 +0200

    changes

--- src/meaning.js
+++ src/meaning.js
@@ -1,21 +1,26 @@
 import React from "react";
-import "./Meaning.css";
-import Synonyms from "./Synonyms";
+import Synonyms from "./Synonyms"
+import "./meaning.css";
 
 export default function Meaning(props) {
-  return (
-    <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <div className="definition">{definition.definition}</div>
 
-            <div className="example">{definition.example}</div>
-            <Synonyms synonyms={definition.synonyms} />
-          </div>
+return(
+    <div className="Meaning">
+        <h4>{props.meaning.partOfSpeech}</h4>
+        {props.meaning.definitions.map( function (definition, index)
+        {return(
+            <div key={index}>
+                <div className="definition">
+                {definition.definition}</div>
+
+                <div className="example">
+                {definition.example}</div>
+                
+                <Synonyms synonyms={definition.synonyms} />
+                <br />
+            </div>
         );
-      })}
+        })} <br />
     </div>
-  );
+);
 }
