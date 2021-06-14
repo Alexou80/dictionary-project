fc9ba718c86a0da4dce4463c3c3612ef93ef18fa
Alexou80 <84702060+Alexou80@users.noreply.github.com>
2021-06-14 11:33:40 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,9 +14,7 @@ export default function Dictionary(props) {
     setResults(response.data[0]);
   }
 
-  function handlePexelsResponse(response) {
-    setPhotos(response.data.photos);
-  }
+  
 
   function search() {
     // documentation: https://dictionaryapi.dev/e
